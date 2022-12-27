b3bd94feae96188d72e4c8d8aa42d1d9b80fdb18
rich_babushka <latatsvt@gmail.com>
2022-12-27 21:45:06 +0800

    Task_01 created

--- src/Task_01.java
+++ src/Task_01.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class Task_01 {
-}
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter a number: ");
+            int number = input.nextInt();
+            if (number %2==0){
+                System.out.println("identify("+number+") -> \"Odd\"");
+            }else{
+                System.out.println("identify("+number+") -> \"Even\"");
+            }
+
+        }
+    }
+
+    
